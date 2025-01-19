bc657ca0772a5758f20d4d9542e1e1079aa0bf91
oleg_sfn <olegmaster1005@gmail.com>
2025-01-19 12:38:44 +0300

    Added cooldown to attack, so animation blending don't trigger attack many times

--- Assets/Scripts/PlayerStates/PlayerAttackingState.cs
+++ Assets/Scripts/PlayerStates/PlayerAttackingState.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Inventory;
 using Items;
 using Managers;
 using UnityEngine;
@@ -10,8 +9,11 @@ namespace PlayerStates
     {
         public override string Name => "Attacking";
 
-        private Collider2D toolCollider;
+        private readonly Collider2D toolCollider;
         private readonly ContactFilter2D contactFilter;
+        
+        private const float AttackCooldown = 0.01f;
+        private float _lastAttackTime;
         
         public PlayerAttackingState(PlayerContoller playerContoller, Animator animator, Rigidbody2D rigidbody2D, Collider2D toolCollider) : base(playerContoller, animator, rigidbody2D)
         {
@@ -32,21 +34,32 @@ namespace PlayerStates
             base.OnAnimationEvent(animationEvent);
             if (animationEvent.stringParameter == "ProcessAttack")
             {
-                List<Collider2D> colliders = new List<Collider2D>();
-                toolCollider.Overlap(contactFilter, colliders);
-
-                Sword sword = GameManager.Instance.inventory.CurrentActiveItem.GetComponent<Sword>();
-                foreach (var col in colliders)
-                {
-                    if (col.CompareTag("Enemy"))
-                    {
-                        col.GetComponent<HealthController>().TakeDamage(sword.swordData.damage);
-                    }
-                }
+                Attack();
             }
             else if (animationEvent.stringParameter == "StopAttacking")
             {
                 PlayerContoller.IsAttacking = false;
+            }
+        }
+
+        private void Attack()
+        {
+            if (Time.time - _lastAttackTime < AttackCooldown)
+            {
+                return;
+            }
+            
+            List<Collider2D> colliders = new();
+            toolCollider.Overlap(contactFilter, colliders);
+
+            var sword = GameManager.Instance.inventory.CurrentActiveItem.GetComponent<Sword>();
+            foreach (var col in colliders)
+            {
+                if (col.CompareTag("Enemy"))
+                {
+                    col.GetComponent<HealthController>().TakeDamage(sword.swordData.damage);
+                    _lastAttackTime = Time.time;
+                }
             }
         }
     }
